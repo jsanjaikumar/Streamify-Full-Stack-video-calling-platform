84b5e6283ca734742605be8d3a6bf14562a9112c
Sanjai Kumar <youremail@example.com>
2025-09-24 22:19:05 +0530

    axios

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -4,6 +4,7 @@ const BASE_URL =
   import.meta.env.MODE === "development"
     ? "http://localhost:5001/api"
     : "https://streamify-full-stack-video-calling.vercel.app/api";
+    
 
 export const axiosInstance = axios.create({
   baseURL: BASE_URL,
